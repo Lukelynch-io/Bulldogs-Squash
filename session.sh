38205a645794c9c5eb202b9d016196ca3ec22795
Luke Lynch <luke@lukelynch.io>
2024-11-25 23:48:40 +1100

    updated session script

--- session.sh
+++ session.sh
@@ -4,7 +4,11 @@ session="bulldogs"
 
 tmux new-session -d -s $session
 
-tmux rename-window -t 1 'Main'
-tmux new-window -t $session:2 -n 'Cli'
-tmux send-keys -t 'Main' 'zsh' C-m 'clear' C-m
+tmux rename-window -t 1 'Webservice'
+tmux send-keys -t 'Webservice' 'cd webservice' C-m 'clear' C-m
+tmux new-window -t $session:2 -n 'CLI'
+tmux new-window -t $session:3 -n 'Site'
+tmux send-keys -t 'Site' 'cd site' C-m 'clear' C-m
+tmux new-window -t $session:4 -n 'bunrundev'
+tmux send-keys -t 'bunrundev' 'cd site && bun run dev' C-m
 tmux attach -t bulldogs
