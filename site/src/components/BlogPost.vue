f81dd7166cd129f12c26a8782fc0f72b1bd116d6
Luke Lynch <luke@lukelynch.io>
2024-11-26 23:02:33 +1100

    Started working on the site

---
+++ site/src/components/BlogPost.vue
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<template>
+  <h2>Blog Title</h2>
+  <p>Blog Description</p>
+</template>
---
+++ site/src/components/LandingPage.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import BlogPost from './BlogPost.vue'
+</script>
+
+<template>
+  <h1>This is the landing page</h1>
+  <BlogPost />
+</template>
