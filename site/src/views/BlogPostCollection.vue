64ec0cfe4db95aed7c046f98edd524d9f3dde244
Luke Lynch <luke@lukelynch.io>
2024-12-26 15:41:24 +1100

    Made it so that the alert box pops up when api has an error

--- site/src/views/BlogPostCollection.vue
+++ site/src/views/BlogPostCollection.vue
@@ -2,24 +2,31 @@
 import { onMounted, ref } from 'vue';
 import type BlogPostData from '@/datatypes/BlogPost';
 import BlogPost from '../components/BlogPost.vue';
+import AlertBox from '@/components/AlertBox.vue';
+import { MessageType } from '@/datatypes/MessageType';
 
 const blogPosts = ref<BlogPostData[]>([]); // Reactive array for blog posts
+const errorOccurred = ref(false);
+const errorMessage = ref('')
+const errorTitle = ref('Error Title')
 
 function RefreshBlogPosts() {
   // Fetch blog posts from the API
   fetch("/api/blogposts")
     .then((response) => response.json())
     .then((data: BlogPostData[]) => {
+      errorOccurred.value = false;
       blogPosts.value = data; // Update the reactive array
     })
     .catch((error) => {
-      console.error("Error fetching blog posts:", error);
+      errorOccurred.value = true;
+      errorMessage.value = "Error fetching blog posts: " + error;
     });
 }
 
 onMounted(() => {
   RefreshBlogPosts();
-  setInterval(RefreshBlogPosts, 100000);
+  setInterval(RefreshBlogPosts, 10000);
 })
 
 </script>
@@ -28,6 +35,8 @@ onMounted(() => {
   <div class="card-collection">
     <BlogPost v-for="(post, index) in blogPosts" :key=index v-bind="post" />
   </div>
+  <AlertBox v-if="errorOccurred" :messageType="MessageType.Info" :messageTitle="errorTitle"
+    :messageDescription="errorMessage" />
 
 </template>
 
