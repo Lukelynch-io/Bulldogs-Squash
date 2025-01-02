b4631a2737c120ada1c277cbf57723fea1322a55
Luke Lynch <luke@lukelynch.io>
2025-01-02 17:34:49 +1100

    Setup dockerfile for persistent database and dockercompose

--- webservice/testdata/setup_testcontainer.sql
+++ webservice/testdata/setup_testcontainer.sql
@@ -1,23 +1,12 @@
 -- Connect as the default PostgreSQL user and database
 -- Replace "postgres" with your desired username and database if needed
 
--- Create a user named 'docker'
-CREATE USER docker;
-
--- Create a database named 'docker'
--- CREATE DATABASE docker;
-
--- Grant all privileges on the 'docker' database to the 'docker' user
-GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
-
--- Connect to the 'docker' database to execute further commands
-\c docker
-
+\c testdb;
 -- Create the 'testdb' table if it doesn't already exist
-CREATE TABLE IF NOT EXISTS testdb (
+CREATE TABLE newtable (
     id INT,
     name VARCHAR(255)
 );
 
 -- Insert a record into the 'testdb' table
-INSERT INTO testdb (id, name) VALUES (1, 'test');
+INSERT INTO newtable (id, name) VALUES (1, 'test');
