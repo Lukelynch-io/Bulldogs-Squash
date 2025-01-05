1808f8bd87896e0a08d31b44e626897e44436cec
Luke Lynch <luke@lukelynch.io>
2025-01-05 12:35:02 +1100

    Updating setup sql script

--- webservice/testdata/setup_testcontainer.sql
+++ webservice/testdata/setup_testcontainer.sql
@@ -1,11 +1,11 @@
 -- Replace "postgres" with your desired username and database if needed
 
-\c testdb
+-- \c testdb
 -- Create the 'testdb' table if it doesn't already exist
-CREATE TABLE testdb (
+CREATE TABLE hello (
     id INT,
     name VARCHAR(255)
 );
 
 -- Insert a record into the 'testdb' table
-INSERT INTO testdb (id, name) VALUES (1, 'test');
+INSERT INTO hello (id, name) VALUES (1, 'test');
