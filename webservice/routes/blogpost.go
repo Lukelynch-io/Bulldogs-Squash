5b5009a54e9a68f63f1ea4c64ac46368810317aa
Luke Lynch <luke@lukelynch.io>
2025-01-09 01:14:34 +1100

    Blogpost endpoint now takes a multipart-form

--- webservice/routes/blogpost.go
+++ webservice/routes/blogpost.go
@@ -1,9 +1,9 @@
 package routes
 
 import (
+	"mime/multipart"
 	"net/http"
 	"webservice/env"
-	"webservice/pkg/auth"
 	"webservice/pkg/post"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +11,7 @@ import (
 
 func LoadBlogPostRoutes(router *gin.Engine) {
 	router.GET("/blogposts", getBlogPosts)
-	router.POST("/blogposts", BearerTokenMiddleware, addBlogPost)
+	router.POST("/blogposts", addBlogPost)
 }
 
 func getBlogPosts(c *gin.Context) {
@@ -19,15 +19,20 @@ func getBlogPosts(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, blogRepo.GetBlogs())
 }
 
-func addBlogPost(c *gin.Context) {
-	blogRepo := c.MustGet(env.PostRepo).(post.PostStorage)
-	userClaims := c.MustGet(env.TokenClaims).(auth.ClaimArray)
-	var newBlogPost post.Post
+type PostWithFile struct {
+	PostTitle       string                `form:"postTitle"`
+	PostDescription string                `form:"postDescription"`
+	FileData        *multipart.FileHeader `form:"imageFile"`
+}
 
-	if err := c.BindJSON(&newBlogPost); err != nil {
-		c.Status(http.StatusInternalServerError)
-		return
+func addBlogPost(c *gin.Context) {
+	// blogRepo := c.MustGet(env.PostRepo).(post.PostStorage)
+	// userClaims := c.MustGet(env.TokenClaims).(auth.ClaimArray)
+	var newBlogPost PostWithFile
+
+	if err := c.ShouldBind(&newBlogPost); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
-	post.InsertPost(blogRepo, newBlogPost, userClaims.IntoMap())
+	c.SaveUploadedFile(newBlogPost.FileData, "/Users/lukelynch/Desktop/hello.png")
 	c.Status(http.StatusCreated)
 }
